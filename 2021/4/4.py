25047e67043f97b5fb9143376f7421bea66e6af2
chriz <chris.dorn@freenet.de>
2021-12-04 12:17:13 +0100

    added day 4

---
+++ 2021/4/4.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def checkWin(board):
+    # check rows
+    for row in board:
+        if row.count(-1) == 5:
+            # bingo
+            return True
+    # check columns
+    transposed = np.array(board).T.tolist() # numpy whyever converts the -1 to a string
+    for row in transposed:
+        if row.count("-1") == 5:
+            # bingo
+            return True
+    return False
+
+def calculateScore(board, draw):
+    sum = 0
+    for row in board:
+        for number in row:
+            if number != -1:
+                sum += int(number)
+    sum *= int(draw)
+    return sum
+
+# read file
+lines = []
+with open("input.txt") as file:
+    lines = file.readlines()
+# Parse input
+draws = lines[0]
+draws = draws.strip('\n')
+draws = draws.split(',')
+
+# get boards
+i = -1
+for line in lines[1:]:
+    if line == '\n':
+        boards.append([])
+        i += 1
+    else:
+        line = line.strip('\n')
+        boards[i].append(line.split(' '))
+# cleanup parsed content
+for board in boards:
+    for row in board:
+        while('' in row):
+            row.remove('')
+        while('\n' in row):
+            row.remove('\n')
+
+
+# Iterate over
+winners = []
+for draw in draws:
+    for board in boards:
+        for row in board:
+            for i in range(len(row)):
+                if draw == row[i]:
+                    row[i] = -1
+        if checkWin(board):
+            print("Next winner", calculateScore(board, draw))
+            winners.append(board)
+    if len(winners) != 0:
+        for winner in winners:
+            boards.remove(winner)
+        winners.clear()
