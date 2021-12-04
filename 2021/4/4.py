97085630b9ee31e4d9bf8f3f10b09211c1602ba6
chriz <chris.dorn@freenet.de>
2021-12-04 12:25:10 +0100

    improved readability

--- 2021/4/4.py
+++ 2021/4/4.py
@@ -23,45 +23,51 @@ def calculateScore(board, draw):
     sum *= int(draw)
     return sum
 
-# read file
-lines = []
-with open("input.txt") as file:
-    lines = file.readlines()
-# Parse input
-draws = lines[0]
-draws = draws.strip('\n')
-draws = draws.split(',')
+def parseInput(filename):
+    # read file
+    lines = []
+    with open(filename) as file:
+        lines = file.readlines()
+    # Parse input
+    draws = lines[0]
+    draws = draws.strip('\n')
+    draws = draws.split(',')
 
-# get boards
-i = -1
-for line in lines[1:]:
-    if line == '\n':
-        boards.append([])
-        i += 1
-    else:
-        line = line.strip('\n')
-        boards[i].append(line.split(' '))
-# cleanup parsed content
-for board in boards:
-    for row in board:
-        while('' in row):
-            row.remove('')
-        while('\n' in row):
-            row.remove('\n')
-
-
-# Iterate over
-winners = []
-for draw in draws:
+    # get boards
+    boards = []
+    i = -1
+    for line in lines[1:]:
+        if line == '\n':
+            boards.append([])
+            i += 1
+        else:
+            line = line.strip('\n')
+            boards[i].append(line.split(' '))
+    # cleanup parsed content
     for board in boards:
         for row in board:
-            for i in range(len(row)):
-                if draw == row[i]:
-                    row[i] = -1
-        if checkWin(board):
-            print("Next winner", calculateScore(board, draw))
-            winners.append(board)
-    if len(winners) != 0:
-        for winner in winners:
-            boards.remove(winner)
-        winners.clear()
+            while('' in row):
+                row.remove('')
+            while('\n' in row):
+                row.remove('\n')
+    return draws, boards
+
+def playBingo(draws, boards):
+    # Iterate over
+    winners = []
+    for draw in draws:
+        for board in boards:
+            for row in board:
+                for i in range(len(row)):
+                    if draw == row[i]:
+                        row[i] = -1
+            if checkWin(board):
+                print("Next winner", calculateScore(board, draw))
+                winners.append(board)
+        if len(winners) != 0:
+            for winner in winners:
+                boards.remove(winner)
+            winners.clear()
+
+draws, boards = parseInput("input.txt")
+playBingo(draws, boards)
