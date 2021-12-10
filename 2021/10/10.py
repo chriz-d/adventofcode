24dc3f24fb3253539d26b6666a5337f66adf9366
chriz <chris.dorn@freenet.de>
2021-12-10 11:41:35 +0100

    added day 10

---
+++ 2021/10/10.py
@@ -0,0 +1,30 @@
+lines = []
+with open("input.txt") as file:
+    lines = [x.strip('\n') for x in file.readlines()]
+
+matching = {')' : '(', ']' : '[', '}' : '{', '>' : '<'}
+matching2 = {'(' : ')', '[' : ']', '{' : '}', '<' : '>'}
+syntaxScoreTable = {')' : 3, ']' : 57, '}' : 1197, '>' : 25137}
+autoCompleteScoreTable = {')' : 1, ']' : 2, '}' : 3, '>' : 4}
+
+syntaxScore = 0
+autoCompleteScoreList = []
+for line in lines:
+    stack = []
+    for i in range(len(line)):
+        if line[i] in matching2.keys():
+            stack.append(line[i])
+        elif matching[line[i]] == stack[-1]:
+            stack.pop()
+        else:
+            syntaxScore += syntaxScoreTable[line[i]]
+            break
+        if i == (len(line) - 1):
+            autoCompleteScore = 0
+            for j in range(len(stack) - 1, -1, -1):
+                autoCompleteScore *= 5
+                autoCompleteScore += autoCompleteScoreTable[matching2[stack[j]]]
+            autoCompleteScoreList.append(autoCompleteScore)
+autoCompleteScoreList.sort()
+print(syntaxScore)
+print(autoCompleteScoreList[int(len(autoCompleteScoreList) / 2)])
